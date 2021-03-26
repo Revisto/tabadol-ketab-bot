c8ef559b3557f252e4dc3218d1efad04f9fcc655
Revisto <walt.shabani@gmail.com>
2021-03-26 14:52:22 +0430

    add search_for_a_book function

--- tabadol-ketab/models.py
+++ tabadol-ketab/models.py
@@ -1,6 +1,25 @@
 import requests
+from bs4 import BeautifulSoup
 import setting
 
 class TabadolKetab:
     def __init__(self):
         self.tabadol_ketab_search_url = "https://book.tabadolketab.com/searched?bookname={book_name}&motarjem=&moalef=&nasher=&categories=&conditionType=AND"
+
+    def search_for_a_book(self, book_name):
+        request = requests.get(self.tabadol_ketab_search_url.format(book_name=book_name))
+        soup = BeautifulSoup(request.content, features="lxml")
+        found_books_elements = soup.find_all("div", {"class": "tb-index-book"})
+        books = []
+        for book_element in found_books_elements:
+            book_name = book_element.find("h3", {"class": "tb-book-title"}).text
+            book_details = book_element.find("div", {"class": "tb-details-book"}).text
+            book_details = book_details.replace("  ","")
+            book_details = book_details.replace("\n","")
+            book_details = book_details.replace("][","\n")
+            book_details = book_details.replace("]","").replace("[","")
+            books.append({"book_name": book_name, "book_details": book_details})
+        
+        return books
+
+TabadolKetab().search_for_a_book("کی")
